395da1bd0238b1faa4a50c651d03f2781ca5bcd4
sriyashetty <118355058+sriyashetty@users.noreply.github.com>
2024-11-21 05:19:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ storage/framework/views/b288ac14ade320b5d5e8a7c06b91d0302ada8efa.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+
+     ©  <?php echo e(date('Y')); ?> Simple Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Sriya Ranga</span>.
+</footer><?php /**PATH C:\attendacelaravel\AttendanceMS-Laravel\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
